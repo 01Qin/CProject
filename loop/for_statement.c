7d7cfb86df5e1dcea6aa33deb21a55facbd7d2ce
01Qin <sisukasqin@hotmail.com>
2025-05-29 10:30:25 +0300

    add for_statement

---
+++ loop/for_statement.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    
+    // for(initial_value; condition; increment)
+       //statement
+    for (i = 1; i <= 5; i++)
+    {
+        printf("%d\n", i);
+    }
+    return 0;
+}
