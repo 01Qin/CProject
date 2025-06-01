4de53dc17df1e3eea26995a25e6247ad98285702
01Qin <sisukasqin@hotmail.com>
2025-06-01 11:21:20 +0300

    add calculate_area

---
+++ functions/calculate_area.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#define PI 3.14159
+
+float calculate_area(float radius);
+
+int main()
+{
+    float circle_area, circle_radius;
+
+    printf("Enter the radius of the circle: ");
+    scanf("%f", &circle_radius);
+
+    circle_area = calculate_area(circle_radius);
+
+    printf("THe area of the circle is: %f", circle_area);
+    return 0;
+
+}
+
+float calculate_are(float radius)
+{
+    float area;
+    area = PI * radius * radius;
+    return area;
+}
+
+float calculate_area(float radius)
+{
+    return 0.0f;
+}
