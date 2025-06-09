a2c5668de08514c0a77341283a6e491f633e6eed
01Qin <sisukasqin@hotmail.com>
2025-06-09 11:49:30 +0300

    edit main program

--- Match3/main.c
+++ Match3/main.c
@@ -1,5 +1,5 @@
-#include "ranlib.h"
-#include <stdio.h>
+#include "raylib.h"
+#include <stdbool.h>
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
@@ -22,10 +22,63 @@ char random_tile()
 
 void init_board()
 {
-    for (int y = 0; y < BOARD_SIZE; y ++)
+    for (int y = 0; y < BOARD_SIZE; y++)
     {
-        for (int x = 0; x < BOARD_SIZE; x ++){
+        for (int x = 0; x < BOARD_SIZE; x++){
             board [y][x] = random_tile();
         }
     }
+}
+
+int main(void)
+{
+    const int screen_width = 800;
+    const int screen_height = 450;
+    InitWindow(screen_width, screen_height, "MATCH3");
+    SetTargetFPS(60);
+    srand(time(NULL));
+
+    init_board();
+
+    while(!WindowShouldClose())
+    {
+        // update game logic
+
+        BeginDrawing();
+        CLearBackground(BLACK);
+
+        // draw tiles
+        for (int y = 0; y < BOARD_SIZE; y++)
+        {
+            for (int x = 0; x < BOARD_SIZE; x++)
+            {
+                Rectangle rect = {
+                    x * TILE_SIZE,
+                    y * TILE_SIZE,
+                    TILE_SIZE,
+                    TILE_SIZE,
+                };
+
+                DrawRectangleLinesEx(rect, 1, DARKGRAY);
+
+                DrawTextEx(
+                    GetFontDefault(),
+                    TextFormat("%c", board[y][x]),
+                    (Vector2){
+                        rect.x + 12, rect.y +8
+                    },
+                    20, 1, WHITE
+
+                );
+
+            };
+        }
+
+        EndDrawing();
+
+    }
+    ClosingWindow();
+    return 0;
+
+
 }
