d8936f797a075ad60aa539dc97897cbb6fd479f5
01Qin <sisukasqin@hotmail.com>
2025-06-21 23:14:04 +0300

    update work_hours

--- Exercises/arrays/work_hours.c
+++ Exercises/arrays/work_hours.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
 
-int enter_days; 
-float enter_1h, enter_2h, enter_3h;
+#define max_days 30
+
 
 int main()
 {
+    int enter_days, i; 
+    float daily_hs[max_days], total_hs = 0.0, avarage_hs;
     printf("The program calculates the total hours worked during\na specific period and the average length of a day.");
     printf("How many days:");
     scanf("%d", &enter_days);
 
-    printf("Enter the working hours for day 1:");
-    scanf("%d", &enter_1h);
-    printf("Enter the working hours for day 2:");
-    scanf("%d", &enter_2h);
-    printf("Enter the working hours for day 3:");
-    scanf("%d", &enter_3h);
+    printf("Enter the working hours for day %d:", i + 1);
+    scanf("%d", &daily_hs[i]);
+
+
+
+ 
+
+
+    printf("Total hours worked: %.1f", total_hs);
+    printf("Average length of day: %.1f", avarage_hs);
+    printf("Hours entered: %.1f", daily_hs);
 
 }
+
