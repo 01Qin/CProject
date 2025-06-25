711157cba7199375602996fa498ea1151043e676
01Qin <sisukasqin@hotmail.com>
2025-06-26 00:29:40 +0800

    Add handle_matrices

---
+++ Exercises/arrays/handle_matrices.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int sum, y, x;
+    int matrix[5][5] =
+    {
+    4, 6, 25, 88, 5,
+    34, 5, 300, 4, 65,
+    78, 43, 11, 90, 125,
+    98, 585, 12, 63, 21,
+    45, 35, 9, 5, 1
+    };
+
+    for (y = 0; y < 5, y++){
+
+        for (x = 0; x < 5; x++){
+
+        }
+    }
+
+    printf("the sum of the elements is ", sum);
+    scanf("%d", &sum);
+}
