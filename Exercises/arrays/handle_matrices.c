d781911ea75566c06f656d7c0f04bd645cee919d
01Qin <sisukasqin@hotmail.com>
2025-07-01 21:43:56 +0800

    Update sum

--- Exercises/arrays/handle_matrices.c
+++ Exercises/arrays/handle_matrices.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    int sum, y, x;
+    int sum = 0, y, x;
     int matrix[5][5] =
     {
     4, 6, 25, 88, 5,
@@ -12,13 +12,22 @@ int main()
     45, 35, 9, 5, 1
     };
 
-    for (y = 0; y < 5, y++){
+    printf("In the array:\n");
+    for (y = 0; y < 5; y++){
 
         for (x = 0; x < 5; x++){
+            if (x == 4){
+                printf("%d\n",matrix[y][x]);
+            }
+            else {
+                printf("%d\t", matrix[y][x]);
+            }
+            sum += matrix[y][x];
 
         }
     }
 
-    printf("the sum of the elements is ", sum);
-    scanf("%d", &sum);
+    printf("the sum of the elements is %d", sum);
+    
+    return 0;
 }
