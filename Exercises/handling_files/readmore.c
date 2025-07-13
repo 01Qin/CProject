09bb4971926a24f0905daee41da2cfb14ee94904
01Qin <sisukasqin@hotmail.com>
2025-07-13 23:10:50 +0800

    Add commas

--- Exercises/handling_files/readmore.c
+++ Exercises/handling_files/readmore.c
@@ -4,10 +4,11 @@ int main()
 {
     FILE *file_handle;
     float number1, number2, number3, number4;
+
     if ((file_handle = fopen("data.txt","r")) == NULL){
         printf("FIle not found!"); return 0;
     } else {
-        fscanf(file_handle, "%f %f %f %f", &number1, &number2, &number3, &number4);
+        fscanf(file_handle, "%f, %f, %f, %f", &number1, &number2, &number3, &number4);
         fclose(file_handle);
 
         printf("The numbers %f, %f, %f and %f were found in the file", number1, number2, number3, number4);
