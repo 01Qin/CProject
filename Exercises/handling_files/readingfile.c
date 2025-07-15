11e0a0345c4117ab4894983c26368c0ed7f64db8
01Qin <sisukasqin@hotmail.com>
2025-07-15 23:34:04 +0800

    Add readingfile

---
+++ Exercises/handling_files/readingfile.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int sum;
+    FILE *file_read;
+
+    if (file_read = fopen("numbers.s", "r") == NULL){
+        printf("File not found."); return 0;
+    }
+}
