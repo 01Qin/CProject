88e397d2d1eba27f221ce3231ec814c6beb930e9
01Qin <sisukasqin@hotmail.com>
2025-07-16 23:14:56 +0800

    Update readingfile

--- Exercises/handling_files/readingfile.c
+++ Exercises/handling_files/readingfile.c
@@ -2,10 +2,21 @@
 
 int main()
 {
-    int sum;
+    int sum=0, num1=0, num2=0, num3=0, num4=0;
     FILE *file_read;
 
-    if (file_read = fopen("numbers.s", "r") == NULL){
-        printf("File not found."); return 0;
+    if ((file_read = fopen("numbers.s", "r")) == NULL){
+        printf("File not found."); return 1;
+    } else {
+        fscanf(file_read, "%d %d %d %d", &num1, &num2, &num3, &num4);
+        fclose(file_read);
+
+        printf("Numbers found in the file 'numbers.s':\n");
+        printf("%d, %d, %d and %d\n\n", num1, num2, num3, num4);
+        
+        sum = num1 + num2 + num3 + num4;
+        printf("Sum of the numbers: %d", sum);
+
     }
+    return 0;
 }
