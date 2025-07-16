e6be392c2dde00785f5344caeef462440af83c7c
01Qin <sisukasqin@hotmail.com>
2025-07-16 23:23:54 +0800

    Add variablechar

---
+++ Exercises/handling_strings/variablechar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    char character_variable;
+    character_variable = 'v';
+
+    printf("THe character corresponding to the number %d is %c", character_variable,character_variable);
+    return 0;
+}
