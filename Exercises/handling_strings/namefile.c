049ab6c0e56033046c313b18b8208a05bc505e18
01Qin <sisukasqin@hotmail.com>
2025-07-17 21:24:46 +0800

    Update namefile

--- Exercises/handling_strings/namefile.c
+++ Exercises/handling_strings/namefile.c
@@ -1,17 +1,36 @@
 #include <stdio.h>
 int main()
 {
-    char firstname[15], lastname[20], filename[11];
-    printf("The program saves your first and last name into a file.");
+    char firstname[15+1], lastname[20+1], filename_part1[8+1], filename_part2[3+1], full_filename[8+1+1+3+1];
+    FILE *output_file;
+    printf("The program saves your first and last name into a file.\n");
 
     printf("Enter your first name:");
-    scanf("%s", &firstname[0]);
+    scanf("%15s", firstname);
 
     printf("Enter your last name:");
-    scanf("%s", &lastname[0]);
+    scanf("%20s", lastname);
 
-    printf("Enter your file name:");
+    printf("Enter the first part of your file name:\n");
+    scanf("%8s", filename_part1);
+    printf("Enter the second part of your file name:\n");
+    scanf("%3s", filename_part2);
 
+
+    printf("File where you want to save your name:\n\t");
+    scanf("11%s",  full_filename);
+
+    output_file = fopen(full_filename, "w");
+    if (output_file == NULL)
+    {
+        printf("An error occurred when opening the file!");
+        return 1;
+    } 
+    fprintf(output_file, "First Name: %s\n", firstname);
+    fprintf(output_file, "Last Name: %s\n", lastname);
+    fclose(output_file);
 
     printf("Successfully saved the data!");
+
+    return 0;
 }
