5384c35f610a6df8d8cd162c97a5ac3b0c6f24ec
01Qin <sisukasqin@hotmail.com>
2025-07-17 15:55:35 +0800

    Add comment

--- Exercises/handling_strings/variablechar.c
+++ Exercises/handling_strings/variablechar.c
@@ -3,6 +3,7 @@
 int main()
 {
     char character_variable;
+    // the apostrophe ( ' ) is used for initialising a character variable
     character_variable = 'v';
 
     printf("THe character corresponding to the number %d is %c", character_variable,character_variable);
