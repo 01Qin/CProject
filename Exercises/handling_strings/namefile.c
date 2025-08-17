449e4f559a0e21e2e6ef4a9f36d8fb057aedcfe5
01Qin <sisukasqin@hotmail.com>
2025-08-17 20:27:17 +0300

    Delete part1&2

--- Exercises/handling_strings/namefile.c
+++ Exercises/handling_strings/namefile.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+#include <string.h>
+
 int main()
 {
-    char firstname[15+1], lastname[20+1], filename_part1[8+1], filename_part2[3+1], full_filename[8+1+1+3+1];
+    char firstname[15+1], lastname[20+1], full_filename[256];
+    
     FILE *output_file;
+
     printf("The program saves your first and last name into a file.\n");
 
     printf("Enter your first name:");
@@ -11,14 +15,10 @@ int main()
     printf("Enter your last name:");
     scanf("%20s", lastname);
 
-    printf("Enter the first part of your file name:\n");
-    scanf("%8s", filename_part1);
-    printf("Enter the second part of your file name:\n");
-    scanf("%3s", filename_part2);
 
-
-    printf("File where you want to save your name:\n\t");
-    scanf("11%s",  full_filename);
+    printf("File where you want to save your name:");
+    scanf("%255s", full_filename);
+    
 
     output_file = fopen(full_filename, "w");
     if (output_file == NULL)
@@ -26,11 +26,11 @@ int main()
         printf("An error occurred when opening the file!");
         return 1;
     } 
-    fprintf(output_file, "First Name: %s\n", firstname);
-    fprintf(output_file, "Last Name: %s\n", lastname);
+
+    fprintf(output_file, "%s\n", lastname);
     fclose(output_file);
 
-    printf("Successfully saved the data!");
+    printf("Successfully saved the data!\n");
 
     return 0;
 }
