d467833cd35d76edc1c012de36bcfd5b0c9b28c9
01Qin <sisukasqin@hotmail.com>
2025-08-20 12:23:26 +0300

    Update switch

--- Assignment/exercise_1.c
+++ Assignment/exercise_1.c
@@ -2,8 +2,9 @@
 // Created by Quinn on 19.8.2025.
 //
 #include <stdio.h>
-float bus_price, taxi_price, money;
-int selection;
+float bus_price=0, taxi_price=0, money=0;
+int selection=0;
+
 int main() {
     printf("Enter price of bus ticket: ");
     scanf("%f", &bus_price);
@@ -14,19 +15,24 @@ int main() {
     printf("How much money do you have: ");
     scanf("%f", &taxi_price);
 
-    printf("You have %f euros left.", money);
-
-    printf("Do you want to take\n, 1. bus (3.50 euros)\n,2. taxi (25.70 euros)");
+    printf("Do you want to take\n 1. bus (3.50 euros)\n 2. taxi (25.70 euros)\n");
 
     printf("Enter your selection: ");
     scanf("%d", &selection);
-    printf("Your chose: %d", selection);
-    if (selection == 1) {
-        printf("You have %f euros left", money - bus_price);
-    else
-        {
-            printf("You have %f euros left", money);
+
+
+    switch (selection) {
+        case 1: {
+            printf("Your chose: %d\n", selection);
+            printf("You have %f euros left.", money - bus_price);
+            break;
         }
+        case 2: {
+            printf("Your chose: %d", selection);
+            printf("You have %f euros left", money - taxi_price);
+        }
+            default:
+            printf("You need to walk, bye!");
     }
     return 0;
 }
