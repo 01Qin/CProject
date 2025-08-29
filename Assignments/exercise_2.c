84eec0c26a0db23eeefc37657d5b555a3ee4387e
01Qin <sisukasqin@hotmail.com>
2025-08-29 13:37:42 +0300

    Define array_size

--- Assignments/exercise_2.c
+++ Assignments/exercise_2.c
@@ -2,11 +2,13 @@
 // Created by Quinn on 28.8.2025.
 //
 #include <stdio.h>
+#define array_size 12
 
 int main() {
-    float tax_rate, income_limit, over_limit;
-    float month_salary[12], month_tax[12];
+    float tax_rate = 0.0, income_limit = 0.0, over_limit = 0.0;
+    float month_salary[array_size], month_tax[array_size];
     int index;
+
     printf("Enter tax rate: ");
     scanf("%f", &tax_rate);
     printf("Enter income limit: ");
@@ -15,7 +17,7 @@ int main() {
     scanf("%f", &over_limit);
     over_limit = tax_rate / income_limit;
 
-    for (index = 0; index < 12; index++) {
+    for (index = 0; index < array_size; index++) {
         printf("Enter income for month %d: ", index + 1);
         scanf("%f", &month_salary[index]);
 
@@ -26,7 +28,7 @@ int main() {
         }
     }
     printf("\nmonth  income    tax\n");
-    for (index = 0; index < 12; index++) {
+    for (index = 0; index < array_size; index++) {
         printf("%-7d%-10.2f%.2f\n", index + 1, month_salary[index], month_tax[index]);
     }
     return 0;
